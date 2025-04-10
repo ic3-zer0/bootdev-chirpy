bcab90bc5f1f7142d8ba1237999899d5a8c210bc
Reed Max <reed@chiemgau-net.com>
2025-04-10 14:43:50 +0200

    added: http fileserver

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	s.ListenAndServe()
+}
